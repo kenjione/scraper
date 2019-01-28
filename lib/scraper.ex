8668983e30e8f2c65f687881ae1a92dda8c43d8c
Alex Chernyshev <alex.kenjione@gmail.com>
2019-01-28 03:19:52 +0100

    Clean up

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -1,47 +1,67 @@
 defmodule Scraper do
   use Task
 
-  def start_link(urls \\ ["https://www.coolblue.nl/laptops"]) do
-    server = ChromeRemoteInterface.Session.new()
-    pages = open_pages(server)
+  # def start_link(urls \\ ["https://www.coolblue.nl/laptops"]) do
+  def start_link(urls \\ ["https://www.coolblue.nl/mobiele-telefoons"]) do
+    fetch_pages_pids
+    |> iterate(urls)
+  end
 
-    page_pids = Enum.reduce(pages, [], fn page, acc ->
-      {:ok, page_pid} = ChromeRemoteInterface.PageSession.start_link(page)
-      [page_pid | acc]
-    end)
-
+  defp iterate(pages_pids, urls) do
     urls
     |> Enum.each(fn url ->
-      parsed = URI.parse(url)
-      cond do
-        Regex.match?(~r/coolblue\.nl/, parsed.host) -> Strategies.CoolBlue.pages(url)
-        true -> []
-      end
-      |> Enum.chunk_every(5)
-      |> Enum.each(&(scrape_chunk(&1, page_pids)))
+      find_strategy(url)
+      |> scrape_with_strategy(url, pages_pids)
     end)
   end
 
-  def scrape_chunk(chunk, page_pids) do
+  defp find_strategy(url) do
+    parsed = URI.parse(url)
+    cond do
+      Regex.match?(~r/coolblue\.nl/, parsed.host) -> Strategies.CoolBlue
+      true -> :error
+    end
+  end
+
+  defp scrape_with_strategy(strategy, url, pages_pids) do
+    strategy.pages(url)
+    |> Enum.chunk_every(5)
+    |> Enum.each(&(scrape_chunk(strategy, &1, pages_pids)))
+  end
+
+  defp fetch_pages_pids do
+    ChromeRemoteInterface.Session.new
+    |> open_pages
+    |> Enum.map(&start_session/1)
+    |> Enum.reject(&is_nil/1)
+  end
+
+  defp start_session(page) do
+    case ChromeRemoteInterface.PageSession.start_link(page) do
+      {:ok, page_pid} ->
+        ChromeRemoteInterface.RPC.DOM.enable(page_pid)
+        ChromeRemoteInterface.RPC.Page.enable(page_pid)
+        page_pid
+      _______________ -> nil
+    end
+  end
+
+  defp scrape_chunk(strategy, chunk, pages_pids) do
     chunk
     |> Enum.with_index
     |> Enum.each(fn {url, index} ->
-      page_pid = Enum.at(page_pids, index)
-      Task.start_link(__MODULE__, :scrape, [url, page_pid])
+      page_pid = Enum.at(pages_pids, index)
+      Task.start_link(__MODULE__, :scrape, [url, strategy, page_pid])
     end)
   end
 
-  def scrape(url, page_pid) do
-    ChromeRemoteInterface.RPC.DOM.enable(page_pid)
-    ChromeRemoteInterface.RPC.Page.enable(page_pid)
-
+  def scrape(url, strategy, page_pid) do
     ChromeRemoteInterface.RPC.Page.navigate(page_pid, %{url: url})
-    Strategies.CoolBlue.data(page_pid)
+    strategy.data(page_pid)
   end
 
-  def open_pages(server) do
+  defp open_pages(server) do
     {:ok, pages} = ChromeRemoteInterface.Session.list_pages(server)
-
     for i <- length(pages)..5, i < 5, do: ChromeRemoteInterface.Session.new_page(server)
     case ChromeRemoteInterface.Session.list_pages(server) do
       {:ok, pages} -> pages
